4a5d19663fe8ae27bd9605c3fc464ebe01d1fd99
54Cheryl <cherylhsu246@gmail.com>
2023-02-15 20:24:20 +0800

    week6-vee-validate

--- src/views/front/CartView.vue
+++ src/views/front/CartView.vue
@@ -54,6 +54,73 @@
       </tfoot>
     </table>
   </div>
+  <div class="my-5 row justify-content-center">
+    <VForm ref="form" class="col-md-6" v-slot="{ errors }" @submit="onSubmit">
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <VField
+          id="email"
+          name="email"
+          type="email"
+          class="form-control"
+          placeholder="請輸入 Email"
+          :class="{ 'is-invalid': errors['email'] }"
+          rules="email|required"
+        ></VField>
+        <ErrorMessage name="email" class="invalid-feedback"></ErrorMessage>
+      </div>
+
+      <div class="mb-3">
+        <label for="name" class="form-label">收件人姓名</label>
+        <VField
+          id="name"
+          name="姓名"
+          type="text"
+          class="form-control"
+          :class="{ 'is-invalid': errors['姓名'] }"
+          placeholder="請輸入姓名"
+          rules="required"
+        ></VField>
+        <ErrorMessage name="姓名" class="invalid-feedback"></ErrorMessage>
+      </div>
+
+      <div class="mb-3">
+        <label for="tel" class="form-label">收件人電話</label>
+        <VField
+          id="tel"
+          name="電話"
+          type="text"
+          class="form-control"
+          :class="{ 'is-invalid': errors['電話'] }"
+          placeholder="請輸入手機號碼"
+          :rules="isPhone"
+        ></VField>
+        <ErrorMessage name="電話" class="invalid-feedback"></ErrorMessage>
+      </div>
+
+      <div class="mb-3">
+        <label for="address" class="form-label">收件人地址</label>
+        <VField
+          id="address"
+          name="地址"
+          type="text"
+          class="form-control"
+          :class="{ 'is-invalid': errors['地址'] }"
+          placeholder="請輸入地址"
+          rules="required"
+        ></VField>
+        <ErrorMessage name="地址" class="invalid-feedback"></ErrorMessage>
+      </div>
+
+      <div class="mb-3">
+        <label for="message" class="form-label">留言</label>
+        <textarea id="message" class="form-control" cols="30" rows="10" ></textarea>
+      </div>
+      <div class="text-end">
+        <button type="submit" class="btn btn-danger">送出訂單</button>
+      </div>
+    </VForm>
+  </div>
 </template>
 
 <script>
