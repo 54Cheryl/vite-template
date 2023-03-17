530f25189f61ad276e987182320612af4e94f8fa
54Cheryl <cherylhsu246@gmail.com>
2023-03-18 00:57:35 +0800

    back top

--- src/views/FrontLayout.vue
+++ src/views/FrontLayout.vue
@@ -1,4 +1,5 @@
 <template>
+  <button type="button" class="btn position-fixed p-0 my-4 mx-5 end-0 bottom-0 border-0" style="z-index: 3;" @click.prevent="backTop"><i class="bi bi-arrow-up-circle text-primary fs-1"></i></button>
   <RouterView :key="$route.path"></RouterView>
 </template>
 
@@ -7,6 +8,11 @@ import { RouterView } from 'vue-router'
 export default {
   components: {
     RouterView
+  },
+  methods: {
+    backTop () {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
   }
 }
 </script>
