d5a7f9bc2d0b8d9843fbabb77d9ff5a3b811996f
54Cheryl <cherylhsu246@gmail.com>
2023-03-22 23:12:22 +0800

    fix font size

--- src/views/front/LoginView.vue
+++ src/views/front/LoginView.vue
@@ -30,7 +30,7 @@
           <button type="submit" class="btn btn-lg btn-primary btn-block text-white w-100 rounded-0 Serif-TC mb-2" style="padding-left: 1.5rem;">
             登入
           </button>
-          <router-link to="/" class="btn btn-outline-n500 rounded-0 Serif-TC w-100">回首頁</router-link>
+          <router-link to="/" class="btn btn-outline-n500 rounded-0 Serif-TC w-100 fs-5">回首頁</router-link>
         </div>
       </div>
     </form>
