c9f7eff5dc3c244ba465dff1adfde746bfc67cae
54Cheryl <cherylhsu246@gmail.com>
2023-04-01 16:09:10 +0800

    fix css

--- src/views/front/CartView.vue
+++ src/views/front/CartView.vue
@@ -55,11 +55,11 @@
             </div>
           </div>
           <template v-if="cartNum === 0">
-            <div class="text-center">
-              <p class="px-3 mt-4 Sans-TC fs-5 accent-color">
+            <div class="text-center pb-5">
+              <p class="px-3 mt-3 Sans-TC fs-5 accent-color">
                 您目前尚未選購產品
               </p>
-              <router-link to="/products" class="btn btn-outline-custom Serif-TC col-6 col-md-auto">立即前往選購</router-link>
+              <router-link to="/products" class="btn btn-outline-custom Serif-TC col-6 col-md-auto mb-4">立即前往選購</router-link>
             </div>
           </template>
           <div class="px-3" v-if="cartNum !== 0">
