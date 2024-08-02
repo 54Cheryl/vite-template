6ba88bd5d04268c175edf01ceb443723e76da712
54Cheryl <cherylhsu246@gmail.com>
2024-08-02 14:13:33 +0800

    測試偵測裝置用

--- src/mixins/deviceDetection.js
+++ src/mixins/deviceDetection.js
@@ -1,12 +1,15 @@
 function detectDevice () {
   const userAgent = navigator.userAgent || navigator.vendor || window.opera
+  const userAgentData = navigator.userAgentData
+  console.log(userAgentData)
   // 判斷 iOS 裝置
   console.log(navigator.userAgent)
   console.log(navigator.platform)
   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    return 'mobile' + navigator.maxTouchPoints
+    return 'mobile,' + navigator.platform + navigator.maxTouchPoints
   }
   const isiPad = (navigator.userAgent.match(/(iPad)/) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1))
+  // const isPad = (navigator.userAgent.match(/(iPad)/) || (navigator.platform === 'Linux armv81' && navigator.maxTouchPoints > 1))
 
   if (isiPad) {
     return 'tablet' + navigator.maxTouchPoints
@@ -21,8 +24,10 @@ function detectDevice () {
     return 'mobile' + navigator.maxTouchPoints
   }
 
+  const desktoptest = [navigator, userAgentData]
+  console.log(desktoptest)
   // 其他情況默認為電腦
-  return 'desktop' + '。userAgent：' + navigator.userAgent + '。platform：' + navigator.platform + '。maxTouchPoints：' + navigator.maxTouchPoints
+  return desktoptest
 }
 
 export { detectDevice }
