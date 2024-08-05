b65c091139183432c4704806d1d181b50a8682d5
54Cheryl <cherylhsu246@gmail.com>
2024-08-05 16:10:48 +0800

    調整偵測裝置

--- src/mixins/deviceDetection.js
+++ src/mixins/deviceDetection.js
@@ -1,7 +1,6 @@
 function detectDevice () {
   const userAgent = navigator.userAgent || navigator.vendor || window.opera
   const userAgentData = navigator.userAgentData
-
   // 判斷 iOS 裝置
   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
     return 'mobile'
@@ -17,25 +16,31 @@ function detectDevice () {
     return 'mobile'
   }
 
-  const applePad = (navigator.userAgent.match(/(iPad)/) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1))
-  // const isPad = (navigator.userAgent.match(/(iPad)/) || (navigator.platform === 'Linux armv81' && navigator.maxTouchPoints > 1))
+  const appleiPad = (navigator.userAgent.match(/(iPad)/) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1))
 
-  if (applePad) {
+  if (appleiPad) {
     return 'tablet'
   }
 
   // 特定情況默認為筆電
-  if (userAgentData.platform === 'macOS') {
-    return 'desktop'
-  } else if (userAgentData.platform === 'Windows') {
-    return 'desktop'
+  if (userAgentData) {
+    if (userAgentData.platform === 'macOS') {
+      return 'desktop'
+    } else if (userAgentData.platform === 'Windows') {
+      return 'desktop'
+    }
+  } else {
+    if (navigator.platform === 'Win32') {
+      return 'desktop'
+    } else if (navigator.platform === 'MacIntel') {
+      return 'desktop'
+    }
   }
 
   // 其他情況默認為 Android 平板
   if (navigator.maxTouchPoints > 1) {
     return 'tablet'
   }
-  
   return 'desktop'
 }
 
